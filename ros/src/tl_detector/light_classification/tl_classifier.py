5e75833369c96388eb1e9813be81fa71d34ff14a
bobondemon <bobon@mirlab.org>
2018-04-06 11:13:19 -0700

    using tensorflow object detection api to detecting tl bounding boxes

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,38 +1,32 @@
 import rospy
 from styx_msgs.msg import TrafficLight
 import tensorflow as tf
-import helper
 from os.path import join
 import numpy as np
 import cv2, time
 
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        model_dir = '/home/student/finalProject/Udacity-term3-final'
-        vgg_path = join(model_dir,'vgg')
-        tl_path = join(model_dir,'tl_model/semantic_model_epoch_10')
-        self._num_classes = 2
-        self._min_num_traffic_pixel_ratio = 0.005
-        
-        # vgg_path = './vgg'
-        self._sess = tf.Session()
-        self._input_image, self._keep_prob, l3out, l4out, l7out = helper.load_vgg(self._sess,vgg_path)
-        global_encoder = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-        rospy.loginfo('[CSChen] len(global_encoder)={}'.format(len(global_encoder)))
-        with tf.variable_scope('decoder'):
-            layer_output = helper.layers(l3out, l4out, l7out, self._num_classes)
-        rospy.loginfo("[CSChen] layer_output.shape={}".format(layer_output.shape))
-        
-        vars_decoder = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope='decoder')
-        rospy.loginfo('[CSChen] len(vars_decoder)={}'.format(len(vars_decoder)))
-        saver = tf.train.Saver(var_list = vars_decoder)
-        saver.restore(self._sess, tl_path)
-        rospy.loginfo("[CSChen] Model loaded")
+        # Loading TF Model
+        model_path = '/home/student/finalProject/Udacity-term3-final/share2team/frozen_sim/frozen_inference_graph.pb'
+        detection_graph = tf.Graph()
 
-        logits = tf.reshape(layer_output,(-1,self._num_classes))
-        self._out_softmax = tf.nn.softmax(logits) # batch_idx x all_pixel x num_classes
-        rospy.loginfo("[CSChen] self._out_softmax.shape={}".format(self._out_softmax.shape))
+        with detection_graph.as_default():
+          od_graph_def = tf.GraphDef()
+          with tf.gfile.GFile(model_path, 'rb') as fid:        
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        rospy.loginfo("[CSChen] loaded model.pb")
+
+        self._image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        self._detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        self._detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        self._detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        self._num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        self._sess = tf.Session(graph=detection_graph)
+        rospy.loginfo("[CSChen] Get tensor variables and create session")
 
 
     def get_classification(self, image):
@@ -42,56 +36,28 @@ class TLClassifier(object):
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
         """
-        #TODO implement light color prediction
         # Should get the image data and it's shape first
-        image_h, image_w, c_num = image.shape  # for simulator, 600, 800, 3
-        rospy.loginfo('[CSChen] Before resizing image.shape = {}'.format(image.shape))
+        image_h_original, image_w_original, c_num = image.shape  # for simulator, 600, 800, 3
+        rospy.loginfo('[CSChen] image.shape = {}'.format(image.shape))
         
-        image = np.concatenate([image, np.zeros(shape=(8,800,3))], axis=0)
+        image_expanded = np.expand_dims(image, axis=0)
+        rospy.loginfo('[CSChen] image_expanded.shape = {}'.format(image_expanded.shape))
 
-        # image = cv2.resize(image,(412,316))
-        
-        image_h, image_w, c_num = image.shape  # for simulator, 608, 800, 3
-        rospy.loginfo('[CSChen] After resizing image.shape = {}'.format(image.shape))
-
-        # [Debug]
-        # traffic_pixels = [image[100,100,:], image[150,150,:]]
-        # traffic_pixels = np.array([traffic_pixels], dtype=np.uint8)
-        # rospy.loginfo('[CSChen] traffic_pixels.shape={}; traffic_pixels={}'.format(traffic_pixels.shape, traffic_pixels))
-        # hsv = cv2.cvtColor(traffic_pixels,cv2.COLOR_RGB2HSV)
-        # rospy.loginfo('[CSChen] hsv={}'.format(hsv))
-
-        # rospy.loginfo('[CSChen] image.shape={}'.format(image.shape))
         stime = time.time()
-        out_softmax = self._sess.run(self._out_softmax,{self._keep_prob: 1.0, self._input_image: [image]})
+        # Actual detection.
+        (boxes, scores, classes, num) = self._sess.run(
+          [self._detection_boxes, self._detection_scores, self._detection_classes, self._num_detections],
+          feed_dict={self._image_tensor: image_expanded})
         etime = time.time()
-        rospy.loginfo('[CSChen] After TensorFlow with {}! out_softmax.shape={}'.format(etime-stime,out_softmax.shape))
+        boxes = np.squeeze(boxes)
+        rospy.loginfo('[CSChen] After TensorFlow with {}! len(boxes)={}'.format(etime-stime,len(boxes)))
+        for box in boxes:
+            ymin, xmin, ymax, xmax = box
+            xmin, xmax = xmin*800, xmax*800
+            ymin, ymax = ymin*600, ymax*600
+            rospy.loginfo('[CSChen] boxes (y,x) = from ({},{}) to ({},{})'.format(ymin,xmin,ymax,xmax))
+
+            # TODO: Do color analyzer ...
+
         
-        im_softmax = out_softmax[:, 1].reshape(image_h, image_w) # image_h, image_w
-        ypixels, xpixels = np.nonzero(im_softmax>0.5)
-
-        traffic_pixels = []
-        for yidx,xidx in zip(ypixels,xpixels):
-            traffic_pixels.append(image[yidx,xidx,:])
-        min_num_traffic_pixel = image_h*image_w*self._min_num_traffic_pixel_ratio
-        if (len(traffic_pixels)<min_num_traffic_pixel):
-            rospy.loginfo('[CSChen] UNKNOWN (no traffic light detected)')
-            return TrafficLight.UNKNOWN
-        traffic_pixels = np.array([traffic_pixels], dtype=np.uint8)
-        rospy.loginfo('[CSChen] traffic_pixels.shape={}'.format(traffic_pixels.shape))
-        hsv = cv2.cvtColor(traffic_pixels,cv2.COLOR_RGB2HSV)
-        h, s, v = hsv[0,:,0], hsv[0,:,1], hsv[0,:,2]
-        redh = len([x for x in h if (x<25)])
-        yellowh = len([x for x in h if (25<x<45)])
-        greenh = len([x for x in h if (45<x<65)])
-        light_hlist = [redh, yellowh, greenh]
-        rospy.loginfo('[CSChen] light_hlist={}'.format(light_hlist))
-
-        if np.argmax(light_hlist)==0:
-            rospy.loginfo('[CSChen] RED')
-            return TrafficLight.RED
-        if np.argmax(light_hlist)==1:
-            rospy.loginfo('[CSChen] YELLOW')
-            return TrafficLight.YELLOW
-        rospy.loginfo('[CSChen] GREEN')
-        return TrafficLight.GREEN
+        return TrafficLight.UNKNOWN
