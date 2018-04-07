0f8057000b7fffa7f07e29387d3cadf474c6cc82
bobondemon <bobon@mirlab.org>
2018-04-07 00:41:38 -0700

    Using HSV as color analyzer, only analyze first three bounding boxes

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -29,6 +29,27 @@ class TLClassifier(object):
         rospy.loginfo("[CSChen] Get tensor variables and create session")
 
 
+    def color_analyzer(self,image):
+        rospy.loginfo('[CSChen] np.max(image)={}'.format(np.max(image)))
+        # I think image in ROS is stored in BGR not RGB !!
+        hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+        h, s, v = hsv[:,:,0], hsv[:,:,1], hsv[:,:,2]
+        h, s, v = h.reshape(-1), s.reshape(-1), v.reshape(-1)
+        rospy.loginfo('[CSChen] np.max(h)={}'.format(np.max(h)))
+        redh = len([x for x in h if (x<25)])
+        yellowh = len([x for x in h if (25<x<45)])
+        greenh = len([x for x in h if (45<x<65)])
+        light_hlist = [redh, yellowh, greenh]
+        rospy.loginfo('[CSChen] light_hlist={}'.format(light_hlist))
+        if np.argmax(light_hlist)==0:
+            rospy.loginfo('[CSChen] RED')
+            return TrafficLight.RED
+        if np.argmax(light_hlist)==1:
+            rospy.loginfo('[CSChen] YELLOW')
+            return TrafficLight.YELLOW
+        rospy.loginfo('[CSChen] GREEN')
+        return TrafficLight.GREEN
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
@@ -51,13 +72,21 @@ class TLClassifier(object):
         etime = time.time()
         boxes = np.squeeze(boxes)
         rospy.loginfo('[CSChen] After TensorFlow with {}! len(boxes)={}'.format(etime-stime,len(boxes)))
-        for box in boxes:
+        # TrafficLight.[RED, YELLOW, GREEN, UNKNOWN] represents 0, 1, 2, 3
+        recog_color_list = [0, 0, 0, 0]
+        # We pick up first three boxes which are more confident
+        for i in range(3):
+            box = boxes[i]
             ymin, xmin, ymax, xmax = box
-            xmin, xmax = xmin*800, xmax*800
-            ymin, ymax = ymin*600, ymax*600
+            xmin, xmax = int(xmin*800), int(xmax*800)
+            ymin, ymax = int(ymin*600), int(ymax*600)
             rospy.loginfo('[CSChen] boxes (y,x) = from ({},{}) to ({},{})'.format(ymin,xmin,ymax,xmax))
 
             # TODO: Do color analyzer ...
+            recog_color = self.color_analyzer(image[ymin:ymax,xmin:xmax,:])
+            recog_color_list[recog_color] += 1
 
-        
-        return TrafficLight.UNKNOWN
+        final_predicted_light = np.argmax(recog_color_list)
+        rospy.loginfo('[CSChen] final predicted light state = {}'.format(final_predicted_light))
+        return final_predicted_light
+        # return TrafficLight.UNKNOWN
